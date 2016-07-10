aadfd604cbf9daaedaef0dc6e9309d7416b98d2d
Chris Minnick <chris@minnick.com>
2016-07-09 21:03:37 -0700

    fixing todo path and putting a link on the homepage

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ angular.
 module('todoApp').
 config(['$locationProvider', '$routeProvider',
     function config($locationProvider, $routeProvider) {
-        //$locationProvider.hashPrefix('!');
+        $locationProvider.hashPrefix('!');
 
         $routeProvider.
             when('/', {

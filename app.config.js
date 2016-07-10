f0f96dd99eb6f51b00991a14e1e496b7c92474ad
Chris Minnick <chris@minnick.com>
2016-07-09 21:11:00 -0700

    figuring out links

--- app.config.js
+++ app.config.js
@@ -5,15 +5,12 @@ config(['$locationProvider', '$routeProvider',
         $locationProvider.hashPrefix('!');
 
         $routeProvider.
-            when('/', {
-                templateUrl: 'templates/home-template.html'
-            }).
             when('/todo', {
                 template: '<todo-list></todo-list>'
             }).
             when('/todo/:todoId', {
                 template: '<todo-detail></todo-detail>'
             }).
-            otherwise({redirectTo: '/'});
+            otherwise({redirectTo: '/todo'});
     }
 ]);

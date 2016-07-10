cb8ebda7d98055a9fe189f15ee294815e86dd24b
Chris Minnick <chris@minnick.com>
2016-07-10 07:00:13 -0700

    fixed nav

--- app.config.js
+++ app.config.js
@@ -5,12 +5,15 @@ config(['$locationProvider', '$routeProvider',
         $locationProvider.hashPrefix('!');
 
         $routeProvider.
+            when('/', {
+                templateUrl: 'templates/home-template.html'
+            }).
             when('/todo', {
                 template: '<todo-list></todo-list>'
             }).
             when('/todo/:todoId', {
                 template: '<todo-detail></todo-detail>'
             }).
-            otherwise({redirectTo: '/todo'});
+            otherwise({redirectTo: '/'});
     }
 ]);

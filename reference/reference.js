7977a4925e6ea9c6ae80f32bda945ce2e4d3ea9b
Thomas Hodges <thodges314@gmail.com>
2016-08-02 22:21:04 -0700

    comment

--- reference/reference.js
+++ reference/reference.js
@@ -1,23 +1,19 @@
 angular
 .module('lrRefMod', [])
 .controller('LrReferenceController', function(){
-	this.Data={
-		Title: '',
-		Url: '',
-		Description: ''
-	};
+	this.Data=Data={};
 })
 .directive('lrInReference', function(){
 	return {
 		restrict: 'AE',
 		templateUrl: 'reference_in.html',
-		scope: true
+		scope: {Data: '='}
 	}
 })
 .directive('lrOutReference', function(){
 	return {
 		restrict: 'AE',
 		templateUrl: 'reference_out.html',
-		scope: true
+		scope: {Data: '='}
 	}
 })

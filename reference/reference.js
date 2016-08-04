7c4df20514ae228cc2ebd2c2926146e795646242
Chris Minnick <chris@minnick.com>
2016-08-03 21:22:15 -0700

    using controlleras

--- reference/reference.js
+++ reference/reference.js
@@ -1,25 +1,28 @@
 angular
 .module('lrRefMod', [])
 .controller('LrReferenceController', function(){
-	this.Data=Data={};
-})
+	var Data = this;
+
+});
+
+angular
+.module('lrRefMod')
 .directive('lrInReference', function(){
 	return {
 		restrict: 'AE',
 		templateUrl: 'reference_in.html',
-		controller: LrReferenceController,
-		controllerAs: 'ctrl',
-		scope: true,
-		bindToController: true
+		controller: 'LrReferenceController',
+		controllerAs: 'ctrl'
 	}
-})
+});
+
+angular
+.module('lrRefMod')
 .directive('lrOutReference', function(){
 	return {
 		restrict: 'AE',
 		templateUrl: 'reference_out.html',
-		controller: LrReferenceController,
-		controllerAs: 'ctrl',
-		scope: true,
-		bindToController: true
+		controller: 'LrReferenceController',
+		controllerAs: 'ctrl'
 	}
-})
+});

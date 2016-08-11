1e67f20b860328ddd9b87e66590c73d0cdb802f6
Chris Minnick <chris@minnick.com>
2016-08-10 21:07:22 -0700

    removed todo

--- src/todo-detail/todo-detail.component.js
+++
@@ -1,9 +0,0 @@
-angular.module('todoDetail').
-component('todoDetail', {
-    template: 'TBD: Detail view for <span>{{$ctrl.todoId}}</span>',
-    controller: ['$routeParams',
-        function TodoDetailController($routeParams) {
-            this.todoId = $routeParams.todoId;
-        }
-    ]
-});
--- src/todo-list/todo-list.module.js
+++
@@ -1,1 +0,0 @@
-angular.module('todoList', ['ngRoute']);

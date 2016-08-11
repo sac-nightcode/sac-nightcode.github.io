0fc09e7ff4afa997d495cacbf1e88a884c3ae965
Chris Minnick <chris@minnick.com>
2016-08-10 20:44:11 -0700

    moved source files into src

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 */
 
 module.exports = {
-    entry: './app.js',
+    entry: './src/app.js',
     output: {
         path: './dist/',
         filename: 'js/app.bundle.js'
